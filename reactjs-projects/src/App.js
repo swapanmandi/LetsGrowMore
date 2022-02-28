76bc596a0b879944d958e06143f4802820a99712
swapanmandi <96920652+swapanmandi@users.noreply.github.com>
2022-02-28 23:55:40 +0530

    Add files via upload

---
+++ reactjs-projects/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Todo from './components/todo-list/todo';
+
+
+
+function App() {
+	return (
+	  <div className="App">
+  <Todo/>
+	  </div>
+	)}
+export default App;
+
+
